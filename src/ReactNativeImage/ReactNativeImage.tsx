d215529a2e6d7aaa08b2cff74d8819c40af6d105
gpbaculio <gpbaculio@gmail.com>
2023-03-12 06:08:02 +0800

    finish challenge

--- src/ReactNativeImage/ReactNativeImage.tsx
+++ src/ReactNativeImage/ReactNativeImage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useColorScheme} from 'react-native';
 
 import {DynamicView} from 'src/components';
 import {
@@ -8,8 +9,12 @@ import {
 } from './components';
 
 const ReactNativeImage = () => {
+  const colorScheme = useColorScheme();
+
+  const backgroundColor = colorScheme === 'light' ? '#FFFFFF' : '#333333';
+
   return (
-    <DynamicView backgroundColor="#333333" flex={1}>
+    <DynamicView backgroundColor={backgroundColor} flex={1}>
       <LittleLemonHeader />
       <LittleLemonBody />
       <LittleLemonFooter />
--- src/ReactNativeImage/components/LittleLemonBody.tsx
+++ src/ReactNativeImage/components/LittleLemonBody.tsx
@@ -1,9 +1,14 @@
-import React, {useState} from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
+import React from 'react';
+import {ScrollView, StyleSheet, useColorScheme} from 'react-native';
+
 import {DynamicFastImage, DynamicText, DynamicView} from 'src/components';
+
 const lemon = require('src/assets/lemon.png');
 
 const LittleLemonBody = () => {
+  const colorScheme = useColorScheme();
+  const color = colorScheme === 'light' ? '#FFFFFF' : '#333333';
+
   return (
     <ScrollView
       indicatorStyle="white"
@@ -21,11 +26,11 @@ const LittleLemonBody = () => {
           borderRadius={16}
           mr="l"
         />
-        <DynamicText fontSize={24} color="#FFFFFF">
+        <DynamicText fontSize={24} color={color}>
           Little Lemon
         </DynamicText>
       </DynamicView>
-      <DynamicText textAlign="center" fontSize={21} color="#FFFFFF">
+      <DynamicText textAlign="center" fontSize={21} color={color}>
         Little Lemon is a charming neighborhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. We would love
         to hear your experience with us!
