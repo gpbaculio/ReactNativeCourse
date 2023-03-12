2511a74f4fb117f8402d1d3c07bd98dc4673e461
gpbaculio <gpbaculio@gmail.com>
2023-03-13 05:48:35 +0800

    setup vector icons

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {View} from 'react-native';
 
 import {AppContainer} from 'src/components';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const App = () => {
   return (
     <AppContainer>
-      <View>asd</View>
+      <MaterialCommunityIcons name="account-arrow-up" size={30} color="#900" />
     </AppContainer>
   );
 };
