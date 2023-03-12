13c7503e9edf61e17582134e792f0c4850ca598e
gpbaculio <gpbaculio@gmail.com>
2023-03-13 00:59:50 +0800

    finish challenge

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,12 +1,53 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {ScrollView, StyleSheet} from 'react-native';
 
-const Welcome = () => {
+import {DynamicFastImage, DynamicText, DynamicView} from 'src/components';
+import {logo} from 'src/assets';
+
+export default function WelcomeScreen() {
   return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <DynamicView
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+        m={10}>
+        <DynamicFastImage
+          width={100}
+          height={100}
+          borderRadius={20}
+          source={logo}
+          resizeMode="cover"
+          accessible={true}
+          accessibilityLabel={'Little Lemon Logo'}
+        />
+        <DynamicText
+          fontSize={30}
+          color="#EDEFEE"
+          textAlign="center"
+          paddingRight="xs"
+          paddingLeft="l"
+          paddingTop="xxL"
+          paddingBottom="s">
+          Little Lemon
+        </DynamicText>
+      </DynamicView>
+      <DynamicText
+        padding="l"
+        marginVertical="xs"
+        fontSize={24}
+        color="#EDEFEE"
+        textAlign="center">
+        Little Lemon is a charming neighborhood bistro that serves simple food
+        and classic cocktails in a lively but casual environment. We would love
+        to hear your experience with us!
+      </DynamicText>
+    </ScrollView>
   );
-};
+}
 
-export default Welcome;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333333',
+  },
+});
