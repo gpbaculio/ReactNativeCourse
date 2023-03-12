89327134fdd993c688dc3374b7d76658a6411111
gpbaculio <gpbaculio@gmail.com>
2023-03-13 07:26:42 +0800

    add assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,5 @@
 const logo = require('./logo.png');
+const littleLemonLogo = require('./little-lemon-logo-grey.png');
+const littleLemonLogoGrey = require('./little-lemon-logo.png');
 
-export {logo};
+export {logo, littleLemonLogo, littleLemonLogoGrey};
