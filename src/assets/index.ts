34ce28ad593067c7655be42f55da293f322385c2
gpbaculio <gpbaculio@gmail.com>
2023-03-13 08:30:26 +0800

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,5 @@
 const logo = require('./logo.png');
-const littleLemonLogo = require('./little-lemon-logo-grey.png');
-const littleLemonLogoGrey = require('./little-lemon-logo.png');
+const littleLemonLogoGrey = require('./little-lemon-logo-grey.png');
+const littleLemonLogo = require('./little-lemon-logo.png');
 
 export {logo, littleLemonLogo, littleLemonLogoGrey};
